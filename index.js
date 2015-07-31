5713e3a1e517bb81e3a0b3715c33352bda43ca1e
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2015-07-30 23:27:04 -0400

    Lesson 2 - Basic Routing

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello world')
+var fs = require('fs')
+var _ = require('lodash')
+var users = []
+
+fs.readFile('users.json', {encoding: 'utf8'}, function (err, data) {
+  if (err) throw err
+
+  JSON.parse(data).forEach(function (user) {
+    user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
+    users.push(user)
+  })
+
 })
 
-app.get('/yo', function (req, res) {
-  res.send('YO!')
+app.get('/', function (req, res) {
+  var buffer = ''
+
+  users.forEach(function (user) {
+    buffer += '<a href="/' + user.username + '">' + user.name.full + '</a><br>'
+  })
+  res.send(buffer)
+})
+
+app.get(/big.*/, function (req, res, next) {
+  console.log('BIG USER ACCESS')
+  next()
+})
+
+app.get(/.*dog.*/, function (req, res, next) {
+  console.log('DOGS GO WOOF')
+  next()
+})
+
+app.get('/:username', function (req, res) {
+  var username = req.params.username
+  res.send(username)
 })
 
 var server = app.listen(3000, function () {
